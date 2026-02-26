95b4bd9578e5bdc2329f32c3a470b14dea6f9513
Lawrence De Belen <lbelen@kvc.org>
2026-02-25 20:04:09 -0600

    added script to run both client and server concurrently

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,5 +16,5 @@ app.get("/api/health", (_req, res) => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀🚀🚀 Server running on port ${PORT} 🚀 🚀 🚀 `);
 });
